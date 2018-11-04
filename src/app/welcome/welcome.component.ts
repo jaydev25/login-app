4f0dcf2767fc398afb3d5379c781121422a40a5c
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-11-04 09:56:46 -0800

    final commit

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,13 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  users: any = [];
+  currentUser: any;
+  url: string = 'http://localhost:5000/';
 
   constructor(
     private route: ActivatedRoute,
+    public http: HttpClient,
     private router: Router
   ) { }
 
@@ -17,7 +22,23 @@ export class WelcomeComponent implements OnInit {
     const token = localStorage.getItem('_token');
     if (!token) {
       this.router.navigate(['/signin']);
+    } else {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': token
+        })
+      };
+      this.http.get(this.url + 'login/users', httpOptions).subscribe((data) => {
+        this.loading = false;
+        if (data['success']) {
+          this.users = data['users'];
+          this.currentUser = data['currentUser'];
+        }
+      }, error => {
+      });
     }
+
   }
 
   logout() {
